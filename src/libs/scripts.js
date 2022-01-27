d975880705b38425ce70aea170b5b3d160614f4c
flashgrinder <flashgrinder92@gmail.com>
2022-01-27 11:11:23 +0400

    make adaptive header

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,7 +1,9 @@
+import burgerMenu from '../libs/modules/burger-menu.js';
 import mainSlider from '../libs/modules/main-slider.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
     
+    burgerMenu.init();
     mainSlider.init();
 
 });
